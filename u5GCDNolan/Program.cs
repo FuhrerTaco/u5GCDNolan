f3ac6beeae6260a2268d4c9a07e1745dc6b29fe4
Führer Taco <geniusnolan@hotmail.com>
2018-05-28 06:38:56 -0400

    Wrote the program.

--- u5GCDNolan/Program.cs
+++ u5GCDNolan/Program.cs
@@ -10,6 +10,22 @@ namespace u5GCDNolan
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("First Number: ");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Second Number: ");
+            int num2 = int.Parse(Console.ReadLine());
+            int min = Math.Min(num1, num2);
+            int max = Math.Max(num1, num2);
+            num1 = max;
+            num2 = min;
+            while (num2 > 0) {
+
+                int temp = num1 % num2;
+                num1 = num2;
+                num2 = temp;
+             }
+            Console.WriteLine("GCD is {0}", num1);
+            Console.ReadLine();
         }
     }
 }
